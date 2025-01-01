25ca9edb910096961cab0f9046333261687e85dd
UsamaRafique123578 <usamarafique.se.pucit@gmail.com>
2025-01-01 16:00:04 +0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,13 @@
-import { useState } from 'react'
-import './App.css'
-import Navbar from './components/Navbar'
-import Ads from './components/Ads'
-import MainBody from './components/MainBody'
+import React from "react"
+import Footer from "./footer/PageFooter.jsx"
 
 function App() {
-  
-  
 
   return (
-    <>
-      <Navbar/>
-      <Ads/>
-      <MainBody/>
-    </>
+  <div>
+    
+    <Footer/></div>
+      
   )
 }
 

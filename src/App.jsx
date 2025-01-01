29118c97e111533de9de94d2e4b7b7207fe30423
UsamaRafique123578 <usamarafique.se.pucit@gmail.com>
2025-01-01 18:56:18 +0500

    route
    add routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
-import React from "react"
-import Footer from "./footer/PageFooter.jsx"
+import React from "react";
+import Footer from "./footer/PageFooter.jsx";
+import Navbar from "./components/Navbar.jsx";
+import { Route, Routes } from "react-router-dom";
+import MainBody from "./components/MainBody.jsx"; // Import MainBody
 
 function App() {
-
   return (
-  <div>
-    
-    <Footer/></div>
-      
-  )
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/home" element={<MainBody />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;

9eff254bf3b4e85ad9a180adeec0bc32a11b911c
UsamaRafique123578 <usamarafique.se.pucit@gmail.com>
2025-01-07 18:00:42 +0500

    commit new changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,20 @@ import React from "react";
 import Footer from "./footer/PageFooter.jsx";
 import Navbar from "./components/Navbar.jsx";
 import { Route, Routes } from "react-router-dom";
-import MainBody from "./components/MainBody.jsx"; // Import MainBody
-
+import MainBody from "./components/MainBody.jsx"; 
+import FollowUpQuestions from "./components/followUpQuestion/Question.jsx";
 function App() {
   return (
     <div>
-      <Navbar />
+      {/* <Navbar />
       <Routes>
         <Route path="/home" element={<MainBody />} />
       </Routes>
-      <Footer />
+      <Footer /> */}
+      <FollowUpQuestions/>
     </div>
+    
+
   );
 }
 
---
+++ src/components/followUpQuestion/Question.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+function FollowUpQuestions() {
+  const questions = [
+    "اگر ایک شخص اپنی بیوی کو تین طلاقیں دے دے تو کیا ہوتا ہے؟",
+    "حج کن حالات میں فرض ہوتا ہے؟",
+    "والدین کے ساتھ بدسلوکی کا انجام کیا ہے؟",
+    "نماز چھوڑنے والے اسلامی تعلیمات کیا ہیں؟",
+    "روزے رکھنے کے جسمانی فوائد کیا ہیں؟",
+    "حضورﷺ پر درود بھیجنے کا انعام کیا ہے؟",
+    "حج کی فرضیت کے لیے کن سے شرائط ضروری ہیں؟",
+    "اگر ایک شخص اپنی بیوی کو تین طلاقیں دے دے تو کیا ہوتا ہے؟",
+  ];
+
+  return (
+    <div className="p-6 bg-gray-50 rounded-lg shadow-lg">
+      <h2 className="text-lg font-bold text-gray-800 mb-4">
+        فالو اپ سوال
+      </h2>
+      <div>
+        {questions.map((question, index) => (
+          <button
+            key={index}
+            className="bg-[rgba(251,248,239,1)] text-gray-800 p-2 rounded mb-2 w-full text-left"
+>
+            {question}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default FollowUpQuestions;
